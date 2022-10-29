528c86cd2ee3468437867208683eb83f424de7c2
derek_lee <20820343@student.curtin.edu.au>
2022-10-29 19:04:54 +0400

    feat: added comments on the question & how answer was thought out for duplicate question

--- duplicate.py
+++ duplicate.py
@@ -1,8 +1,18 @@
 class Solution:
+    """
+    Qu:
+    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+    Ans:
+    1) Sorted the list such that any duplicates will be next to each other
+    2) Used an enum for loop to get 2 values for comparison
+    3) As soon as a duplicate happens return True, otherwise return False
+    """
+
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
-        for i,j in enumerate(nums):
-            if i < len(nums)-1 and j == nums[i+1]:
+        for i, j in enumerate(nums):
+            if i < len(nums) - 1 and j == nums[i + 1]:
                 return True
 
         return False

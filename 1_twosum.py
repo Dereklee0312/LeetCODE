63114652bc2cee1cfeeb2d17ca79bd801427b6ea
derek_lee <20820343@student.curtin.edu.au>
2022-10-31 18:54:10 +0400

    Ex 1 - Came up with 2 solutions First one is very slow since it is using nested loops
    Second solution is faster but not at maximum efficiency.
    
    TODO: Improve speed

--- 1_twosum.py
+++ 1_twosum.py
@@ -33,5 +33,45 @@ Constraints:
 Only one valid answer exists.
 """
 
+
 class Solution:
+    """
+    Solution 1:
+    Runtime: 8395 ms
+    Mem Usage: 14.8 MB
+
+    Way too slow because using nested loops and nested conditions.
+
+    - For each number in the list, go through the list again until the addition of the numbers = target
+      but the second number should not be at same index as first number
+    - Goes through each number through the nested loop.
+    - First condition ensures that the indexes do not correspond.
+    - Second condition returns the list of indexes if correct combination is found.
+    """
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            firstNum = nums[i]
+            for j in range(len(nums)):
+                if j != i:
+                    secondNum = nums[j]
+                    if firstNum + secondNum == target:
+                        return [i, j]
+
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Solution 2:
+        Runtime: 1617 ms
+        Mem Use: 14.9 MB
+
+        Still uses a loop, but more efficient since only one loop and condition is used.
+
+        Reverse logic is used: 
+        - For each number in list, find the remainder when substracting from target
+        - Condition checks if the remainder is in the list and check if index of remainder is not same as first number
+        - Return list of the indexes if conditions are satisfied
+        '''
+        for h, i in enumerate(nums):
+            secondNum = target - i
+            if (secondNum in nums) and (nums.index(secondNum) != h):
+                return [h, nums.index(secondNum)]

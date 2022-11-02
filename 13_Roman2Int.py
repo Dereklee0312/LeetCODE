8c0d910b69628cc8ad034c4718626b0fa4ed4c8d
derek_lee <20820343@student.curtin.edu.au>
2022-11-02 18:59:42 +0400

    Ex13: solved by using substraction method

--- 13_Roman2Int.py
+++ 13_Roman2Int.py
@@ -88,5 +88,22 @@ class Solution:
     def romanToInt1(self, s: str) -> int:
         """
         Sol 2:
-            Focus on the aspect of substraction
+        Runtime: 107 ms
+        Mem Use: 14 MB
         """
+
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        total = 0
+        skip = None
+
+        for index, char in enumerate(s[::-1]):
+            if (romans[s[len(s) - index - 2]] < romans[char]) and (index != len(s) - 1):
+                total -= romans[s[len(s) - index - 2]]
+                skip = len(s) - index - 2
+            if skip == None:
+                total += romans[char]
+            elif index != len(s) - skip - 1:
+                total += romans[char]
+
+        return total

a46e8f3a4a5f612c2e8d874ff0c997e06268babe
derek_lee <20820343@student.curtin.edu.au>
2023-01-29 23:59:49 +0400

    347: Added 2nd Soln but unsatisfactory

--- 347.py
+++ 347.py
@@ -30,8 +30,13 @@
 
 
 class Solution:
+    """
+    TIPS FROM Submissions:
+        1 - HEAP DS
+        2 - Counter from collections
+    """
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
+        """
         Solution 1:
         Runtime: 1713 ms
         Mem Usage: 18.7 MB
@@ -40,7 +45,7 @@ class Solution:
         1 - Dictionary ~ Number : Count of num
         2 - Sort dictionary
         3 - Iterate k time and append to list
-        '''
+        """
         # Dictionary
         # num: count of number
         maps = {}
@@ -54,3 +59,13 @@ class Solution:
             lst.append(sort[_][0])
 
         return lst
+
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
+        """
+        Simply refactor of first solution
+        """
+        counts = []
+        for num in set(nums):
+            counts.append((num, nums.count(num)))
+
+        return [x[0] for x in sorted(counts, key=lambda x: x[1], reverse=True)[:k]]

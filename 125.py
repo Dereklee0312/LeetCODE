a918e446c743a2b4eaf146681233b46716b69b4f
derek_lee <20820343@student.curtin.edu.au>
2023-02-04 20:02:20 +0400

    125: Completed

---
+++ 125.py
@@ -0,0 +1,42 @@
+# Author: Derek Lee
+# Start Date: 04-02-2023
+# End Date:
+# Link: https://leetcode.com/problems/valid-palindrome/
+# Difficulty: Easy
+#
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+#
+# Given a string s, return true if it is a palindrome, or false otherwise.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+#
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+#
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 2 * 105
+# s consists only of printable ASCII characters.
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newS = (re.sub(r"[^a-zA-Z0-9]", "", s)).lower()
+
+        return newS[::-1] == newS
